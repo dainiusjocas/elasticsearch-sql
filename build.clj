161e3ab16a1dbef8fab4a9bd7f77ad34edfe7cea
Dainius Jocas <dainius.jocas@gmail.com>
2023-01-17 00:58:10 +0200

    chore: release to clojars scripts (#2)
    * chore: scripts to release library to Clojars

--- build.clj
+++ build.clj
@@ -1,5 +1,31 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]))
+  (:require [clojure.tools.build.api :as b]
+            [org.corfield.build :as bb]))
+
+;; Library deployment to Clojars
+(def lib 'lt.jocas/elasticsearch-sql)
+(defn- the-version [patch] (format "1.0.%s" patch))
+(def version (the-version (b/git-count-revs nil)))
+(def snapshot (the-version "999-SNAPSHOT"))
+
+(defn deploy "Deploy the JAR to Clojars." [opts]
+  (-> opts
+      (assoc :lib lib :version (if (:snapshot opts) snapshot version))
+      (bb/clean)
+      (assoc :src-pom "pom.xml.template")
+      (bb/jar)
+      (bb/deploy)))
+
+(defn trigger-library-release
+  "Creates a git tag `lib-vVERSION` and pushes it to origin.
+  This will trigger GH Action to release to Clojars."
+  [opts]
+  (let [tag (str "lib-v" (if (:snapshot opts) snapshot version))]
+    (println "Initiating release for git tag:" tag)
+    (b/git-process {:git-args ["tag" tag]})
+    (b/git-process {:git-args ["push" "origin" tag]})))
+
+;; Uberjar stuff
 
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"
